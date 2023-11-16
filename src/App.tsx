e17eae5943f22863161fcb9a950e23d1b9ace165
mathisw59 <mathis.wauquiez@gmail.com>
2023-11-16 17:49:09 +0100

    V-1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test de Mathis</h1>
+        <h1 className="App-title">Bienvenue sur mon super blindtest !</h1>
       </header>
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
